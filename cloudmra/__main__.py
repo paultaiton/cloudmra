efe771fe9ffd2e6912f265a8764520cc9920500c
Paul Aiton <paul@aiton.info>
2022-05-15 01:44:58 -0700

    wip stash

--- cloudmra/__main__.py
+++ cloudmra/__main__.py
@@ -7,6 +7,7 @@ import json
 import os
 import argparse
 import signal
+import email
 
 from cloudmra.cloudhandler import cloudhandler
 from cloudmra.aliashandler import aliashandler
@@ -29,6 +30,7 @@ class overseer():
         self.inhandler = cloudhandler(queue_name=QUEUE_NAME, expire_bucket=EXPIRE_BUCKET)
         self.outhandler = deliveryhandler(host=DELIVERY_HOST, port=DELIVERY_PORT)
         self.alias = aliashandler(ALIAS)
+        self.email_parser = email.parser.BytesParser()
 
     def signal_handler(self, sig_int, frame_object=0):
         if sig_int is signal.SIGINT or sig_int is signal.SIGTERM:
@@ -36,13 +38,14 @@ class overseer():
             self.CONTINUE = False
 
     def process(self):
-        emails = True
-        while emails is not False:
-            emails = self.inhandler.fetch()
-            if emails:
-                print("Processing " + str(len(emails)) + " new email messages.")
-                for email in emails:
-                    message, receipients = email[0]
+        email_list = True
+        while email_list:
+            email_list = self.inhandler.fetch()
+            if email_list:
+                print("Processing {0} new messages.".format(len(email_list)))
+                for mail in email_list:
+                    raw_message, receipients = mail[0]
+                    parsed_message = email_parser.parsebytes(raw_message)
                     default = self.alias.get_default(list(receipients)[0])
                     adressee = set()
                     confirmed = set()
@@ -50,15 +53,15 @@ class overseer():
                     for address in receipients:
                         adressee.add(self.alias.get_alias(address))
                     for i in adressee:
-                        if (self.outhandler.deliver(message, i)):
+                        if (self.outhandler.deliver(parsed_message, i)):
                             confirmed.add(i)
                         else:
                             todefault = True
                     if todefault and default not in confirmed:
-                        self.outhandler.deliver(message, default)
+                        self.outhandler.deliver(parsed_message, default)
                         confirmed.add(default)
                     if len(confirmed) > 0:
-                        self.inhandler.delete(email)
+                        self.inhandler.delete(mail)
 
 
 def main(args=None):
@@ -90,7 +93,7 @@ def main(args=None):
     signal.signal(signalnum=signal.SIGTERM, handler=main_overseer.signal_handler)
     signal.signal(signalnum=signal.SIGINT, handler=main_overseer.signal_handler)
 
-    if main_overseer.CONTINUE is False:
+    if not main_overseer.CONTINUE:
         main_overseer.process()
     else:
         while main_overseer.CONTINUE:
