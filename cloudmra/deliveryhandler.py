562b15d67780bf4404072d21008534e6c67aeb3e
Paul Aiton <paul@aiton.info>
2025-03-10 18:45:33 -0700

    compare

--- cloudmra/deliveryhandler.py
+++ cloudmra/deliveryhandler.py
@@ -14,18 +14,31 @@ class deliveryhandler():
         self.password = password
         self.lmtp = smtplib.LMTP(host=host, port=port)  # , user=user, password=password)
 
+    # def deliver(self, message, receipient):
+    #     while True:
+    #         try:
+    #             self.lmtp.send_message(msg=message, from_addr=None, to_addrs=receipient)  # , mail_options=(SMTPUTF8))
+    #         except smtplib.SMTPRecipientsRefused:
+    #             return self.INVALIDUSER
+    #         except (smtplib.SMTPServerDisconnected, smtplib.SMTPSenderRefused):
+    #             self.lmtp.connect(host=self.hostname, port=self.port)
+    #         # except UnicodeEncodeError:  # Do not run with this uncommented, will cause endless loop
+    #         #     print("debug")
+    #         else:
+    #             return True
+
     def deliver(self, message, receipient):
-        while True:
+        try:
+            self.lmtp.send_message(msg=message, from_addr=None, to_addrs=receipient)
+        except smtplib.SMTPRecipientsRefused:
+            return self.INVALIDUSER
+        except (smtplib.SMTPServerDisconnected, smtplib.SMTPSenderRefused):
+            self.lmtp.connect(host=self.hostname, port=self.port)
             try:
-                self.lmtp.send_message(msg=message, from_addr=None, to_addrs=receipient)  # , mail_options=(SMTPUTF8))
+                self.lmtp.send_message(msg=message, from_addr=None, to_addrs=receipient)
             except smtplib.SMTPRecipientsRefused:
                 return self.INVALIDUSER
-            except (smtplib.SMTPServerDisconnected, smtplib.SMTPSenderRefused):
-                self.lmtp.connect(host=self.hostname, port=self.port)
-            # except UnicodeEncodeError:  # Do not run with this uncommented, will cause endless loop
-            #     print("debug")
-            else:
-                return True
+        return True
 
 
 if __name__ == "__main__":
